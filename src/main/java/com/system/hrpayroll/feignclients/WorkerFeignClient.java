8901fa323984ab3534ab4c6a1a6145bf81f6d241
Dalton <dalton@viasoft.com.br>
2021-01-18 16:56:14 -0300

    Feign client

---
+++ src/main/java/com/system/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,23 @@
+package com.system.hrpayroll.feignclients;
+
+//chamando outro microservice, usando a estrutura do spring cloud
+
+import com.system.hrpayroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//namne = nome do projeto a ser chamado
+
+//hardcode, url
+
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+    //mesma assinatura do metodo a ser chamado
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable(value = "id") Long id);
+
+}
--- src/main/java/com/system/hrpayroll/services/PaymentService.java
+++ src/main/java/com/system/hrpayroll/services/PaymentService.java
@@ -2,34 +2,38 @@ package com.system.hrpayroll.services;
 
 import com.system.hrpayroll.entities.Payment;
 import com.system.hrpayroll.entities.Worker;
+import com.system.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Autowired
-    private RestTemplate restTemplate;
+//    @Autowired
+//    private RestTemplate restTemplate;
 
-    @Value("${hr-worker.host}") //pegando dp application,properies
-    private String workerHost;
+//    @Value("${hr-worker.host}") //pegando dp application,properies
+//    private String workerHost;
+
+    @Autowired
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(Long workerId, int days){
 
-        Map<String, String> uriVariables = new HashMap<>(); //map é uma interface, para intanciar tem que usar classe concreta
-        uriVariables.put("id", workerId.toString());
+        // usando o restTemplate
+        //Map<String, String> uriVariables = new HashMap<>(); //map é uma interface, para intanciar tem que usar classe concreta
+        //uriVariables.put("id", workerId.toString());
 
         //chamar no microserviço de worker
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+        //Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        //return new Payment(worker.getName(), worker.getDailyIncome(), days);
 
         //pagamento mockado
         //return new Payment("Bob", 200.0, days);
+
+        //usando o feign
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
 
     }
 
